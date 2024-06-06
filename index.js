eccc4b8565e584c2ee8c362177c6b72c5ff70563
Shatak-the-imposter <helloshatak@gmail.com>
2024-06-07 01:20:09 +0600

    employee delete operation is working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -40,8 +40,17 @@ async function run() {
       const result = await employeCollection.insertOne(employe)
       res.send(result)
     })
+
     app.get('/employesData', async (req, res) => {
       const result = await employeCollection.find().toArray()
+      res.send(result)
+    })
+
+    // delete a employe from db
+    app.delete('/employe/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await employeCollection.deleteOne(query)
       res.send(result)
     })
 
